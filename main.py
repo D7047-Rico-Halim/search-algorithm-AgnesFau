556d706b70046ad51fa8f99ebdd72d85bfbc5bf6
AgnesFau <145894754+AgnesFau@users.noreply.github.com>
2024-09-23 20:06:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ graph = {
 
 # Declare the heuristic values for each node
 heuristics = {
-    'A': 7,
-    'B': 6,
-    'C': 4,
-    'D': 3,
-    'E': 2,
-    'F': 1,
-    'G': 0,
+    "A": 7,
+    "B": 6,
+    "C": 4,
+    "D": 3,
+    "E": 2,
+    "F": 1,
+    "G": 0
 }
